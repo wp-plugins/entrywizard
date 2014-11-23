392b39939dcda240638332e3d12b1c1a01822382
joanne123 <joanne123@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-23 17:41:46 +0000

    tagging version 1.2.6
    git-svn-id: https://plugins.svn.wordpress.org/entrywizard/tags/1.2.6@1031263 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/validation/ewz-webform-input.php
+++ classes/validation/ewz-webform-input.php
@@ -13,20 +13,28 @@ class Ewz_Webform_Input extends Ewz_Input
      function __construct( $form_data ) {
          parent::__construct( $form_data );
          assert( is_array( $form_data ) );
+       
          $this->rules = array(
 
-                  '_wp_http_referer' => array( 'type' => 'string',     'req' => false, 'val' => '' ),
-                  'ewzmode'        =>  array( 'type' => 'fixed',  'req' => true,  'val' => 'webform' ),
-                  'ewznonce'       =>  array( 'type' => 'anonce', 'req' => true,  'val' => '' ),
-                  'layout_id'      =>  array( 'type' => 'seq',    'req' => true,  'val' => '' ),
-                  'page'           =>  array( 'type' => 'fixed',  'req' => true,  'val' => 'entrywizard' ),
-                  'prefix'         =>  array( 'type' => 'v_prefix', 'req' => false,  'val' => '' ),
-                  'upload_open'    =>  array( 'type' => 'bool',   'req' => false,  'val' => '' ),
-                  'o_user'         =>  array( 'type' => 'v_users', 'req' => false,  'val' => '' ),
-                  'webform_id'     =>  array( 'type' => 'seq',    'req' => false,  'val' => '' ),
-                  'webform_ident'  =>  array( 'type' => 'ident',  'req' => true,  'val' => '' ),
-                  'webform_title'  =>  array( 'type' => 'string', 'req' => true,  'val' => '' ),
-                  'openwebform'    =>  array( 'type' => 'seq',    'req' => false,  'val' => '' ),
+                  '_wp_http_referer' => array( 'type' => 'to_string', 'req' => false, 'val' => '' ),
+                  'ewzmode'        =>  array( 'type' => 'fixed',      'req' => true,  'val' => 'webform' ),
+                  'ewznonce'       =>  array( 'type' => 'anonce',     'req' => true,  'val' => '' ),
+                  'layout_id'      =>  array( 'type' => 'to_seq',     'req' => true,  'val' => '' ),
+                  'num_items'      =>  array( 'type' => 'to_seq',     'req' => false, 'val' => '' ),
+                  'page'           =>  array( 'type' => 'fixed',      'req' => true,  'val' => 'entrywizard' ),
+                  'prefix'         =>  array( 'type' => 'v_prefix',   'req' => false, 'val' => '' ),
+                  'apply_prefix'   =>  array( 'type' => 'to_bool',    'req' => false, 'val' => '' ),
+                  'gen_fname'      =>  array( 'type' => 'to_bool',    'req' => false, 'val' => '' ),
+                  'upload_open'    =>  array( 'type' => 'to_bool',    'req' => false, 'val' => '' ),
+                  'o_user'         =>  array( 'type' => 'v_users',    'req' => false, 'val' => '' ),
+                  'webform_id'     =>  array( 'type' => 'to_seq',     'req' => false, 'val' => '' ),
+                  'webform_ident'  =>  array( 'type' => 'ident',      'req' => true,  'val' => '' ),
+                  'webform_title'  =>  array( 'type' => 'to_string',  'req' => true,  'val' => '' ),
+                  'openwebform'    =>  array( 'type' => 'to_seq',     'req' => false, 'val' => '' ),
+                  'auto_close'     =>  array( 'type' => 'to_bool',    'req' => false, 'val' => '' ),
+                  'auto_date'      =>  array( 'type' => 'v_date',     'req' => false, 'val' => '' ),
+                  'auto_time'      =>  array( 'type' => 'v_time',     'req' => false, 'val' => '' ),
+                  'archive_id'    =>  array( 'type' => 'to_string',     'req' => false, 'val' => '' ),
                   );
         $this->validate();
      }
@@ -38,18 +46,55 @@ class Ewz_Webform_Input extends Ewz_Input
         if ( !array_key_exists( 'upload_open', $this->input_data ) ) {
             $this->input_data['upload_open'] = false;
         }
+        if ( !array_key_exists( 'apply_prefix', $this->input_data ) ) {
+            $this->input_data['apply_prefix'] = false;
+        }
+        if ( !array_key_exists( 'gen_fname', $this->input_data ) ) {
+            $this->input_data['gen_fname'] = false;
+        }
+        if ( !array_key_exists( 'auto_close', $this->input_data ) ) {
+            $this->input_data['auto_close'] = false;
+        }
+        if( $this->input_data['auto_close']){
+            $tz_opt = get_option('timezone_string');
+            if( $tz_opt ){
+                date_default_timezone_set( $tz_opt );
+            }
+            $dt = strtotime( $this->input_data['auto_date'] . ' ' . $this->input_data['auto_time']  ); 
+            $now = time();  // current Unix timestamp
+            if( $dt < $now + 30 ){
+                throw new EWZ_Exception( 'Input date too early' );
+            }
+        }
         return true;
      }
 
      //****** All v_.... functions must return a boolean or raise an exception **************/
 
+     function v_date( $value, $arg ){
+         assert( is_string( $value ) || empty( $value ) );
+         assert( $arg == '' );
+         $timestamp = strtotime( $value );
+         if( !$timestamp ){
+             throw new EWZ_Exception( 'Bad input for date' );
+         }
+         return true;
+     }
+     function v_time( $value, $arg ){
+         assert( is_string( $value ) || empty( $value ) );
+         assert( $arg == '' );
+         if( !is_string( $value ) || !preg_match( '/^\d\d:\d\d:\d\d$/', $value ) ){
+             throw new EWZ_Exception( 'Bad input for time' );
+         }
+         return true;
+     }
+
      function v_prefix( $value, $arg ){
          assert( is_string( $value ) || empty( $value ) );
          assert( $arg == '' );
-         if( !is_string( $value ) && preg_match( '/^[\[\]A-Z0-9~\-_]*$/i', $value ) ){
+         if( !is_string( $value ) || !preg_match( '/^[\[\]A-Z0-9~\-_]*$/i', $value ) ){
              throw new EWZ_Exception( 'Bad input for prefix' );
          }
-
          return true;
      }
 
@@ -60,7 +105,7 @@ class Ewz_Webform_Input extends Ewz_Input
              throw new EWZ_Exception( 'Bad input for user' );
          }
          foreach( $value as $key => $uid ){
-             if( !$this->seq( $value[$key], $arg ) ){   // seq potentially changes first arg
+             if( !self::to_seq( $value[$key], $arg ) ){   // seq potentially changes first arg
                  throw new EWZ_Exception( "Bad value '$uid' for user" );
              }
          }
--- entrywizard.php
+++ entrywizard.php
@@ -4,7 +4,7 @@
   Plugin Name: EntryWizard
   Plugin URI: http:
   Description:  Uploading by logged-in users of sets of image files and associated data. Administrators may download the images together with the data in spreadsheet form.
-  Version: 0.9
+  Version: 1.2.6
   Author: Josie Stauffer
   Author URI:
   License: GPL2
@@ -25,20 +25,19 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+
 defined( 'ABSPATH' ) or exit;   // show a blank page if try to access this file directly
 
-define( 'EWZ_PLUGIN_DIR',    plugin_dir_path( __FILE__ ) );
-define( 'EWZ_VERSION', 0.9 );
+define( 'EWZ_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'EWZ_CUSTOM_DIR', plugin_dir_path( __FILE__ ) );
 define( 'EWZ_REQUIRED_WP_VERSION', '3.5' );
-define( 'EWZ_REQUIRED_PHP_VERSION', '5.2.1' );
-
-
-ewz_set_dev_env();     // development environment
+define( 'EWZ_REQUIRED_PHP_VERSION', '5.3.0' );
 
 /*
  * INCLUDES
  */
 require_once( EWZ_PLUGIN_DIR . 'classes/ewz-setup.php' );
+require_once( EWZ_PLUGIN_DIR . 'classes/ewz-webform.php' );
 
 if ( is_admin() ) {
     require_once( EWZ_PLUGIN_DIR . '/includes/ewz-admin.php' );
@@ -46,6 +45,7 @@ if ( is_admin() ) {
 
 // this is needed for admin, too, because the ajax function runs as admin
 require_once( EWZ_PLUGIN_DIR . '/includes/ewz-upload.php' );
+require_once( EWZ_PLUGIN_DIR . '/includes/ewz-followup.php' );
 
 
 /*
@@ -59,91 +59,158 @@ register_deactivation_hook( __FILE__, array( 'Ewz_Setup', 'deactivate_ewz' ) );
 /**
  * ACTIONS
  */
+add_action('plugins_loaded', 'ewz_requires_version');   // first useable hook
+
 add_action( 'wp_enqueue_scripts', 'ewz_add_stylesheet' );
-// in the admin area we add another function after this, so make sure we know its priority
+
 add_action( 'plugins_loaded', 'ewz_init_globals', 10 );
 
+add_action( 'admin_init', 'ewz_check_for_db_updates' );
+        
+// Needed for cron job to auto-close webform
+add_action( 'init', 'Ewz_Webform::schedule_close' );
 
+// delete items when a user is deleted
+add_action( 'deleted_user', 'Ewz_Item::delete_user_items' );
 
 /*
- * SHORTCODE
+ * SHORTCODES
  */
-if ( !is_admin() ) {
+
+/* Set up the EWZShortcodes menu in tinyMCE */
+function ewz_shortcode_menu() {
+    // only hook up these filters if we're in the admin panel, and the current user has permission
+    // to edit posts and pages, and has some entrywizard permissions
+    if ( current_user_can( 'edit_posts' ) && current_user_can( 'edit_pages' ) && Ewz_Permission::can_see_webform_page() ) {
+        add_filter( 'mce_buttons_2', 'ewz_mce_button' );
+        add_filter( 'mce_external_plugins', 'ewz_mce_plugin' );
+
+        foreach( array('post.php','post-new.php') as $hook ){
+            add_action( "admin_head-$hook", 'ewz_admin_head' );
+        }     
+    }
+}
+add_action( 'admin_init',  'ewz_shortcode_menu' );
+
+/* pass the data to javascript */
+function ewz_admin_head(){
+    global $all_text;
+    $ewzdata = array();
+    $ewzdata['webforms'] = array_values(array_filter( Ewz_Webform::get_all_webforms(),
+                                                      "Ewz_Permission::can_view_webform" ));;
+     ?>
+<script type='text/javascript'>
+     var EWZdata =  <?php echo json_encode( $ewzdata ); ?>;
+</script>
+    <?php
+}  
+
+
+/* pass the button data to mce_buttons filter */
+function ewz_mce_button( $buttons ) {
+    assert( is_array( $buttons ) );
+
+    array_push( $buttons,'ewz_shortcodes');
+    return $buttons;
+}
+ 
+/* pass the javascript file name to mce_external_plugins filter */
+function ewz_mce_plugin( $plugins ) {
+    assert( is_array( $plugins ) );
+
+    $plugins['ewz_shortcodes'] = plugins_url() . '/entrywizard/javascript/ewz-shortcodes.js';
+    return $plugins;
+}
+
+
+if ( ! is_admin() ) { 
     // adding this for all admin pages triggers warnings for some themes and plugins
     // just add it specifically for the ajax calls when they are run in ewz-admin.php
     add_shortcode( 'ewz_show_webform', 'ewz_show_webform' );
+    add_shortcode( 'ewz_followup', 'ewz_followup' );
 }
 
 /*
- * LOCALIZATION
+ * UPDATES
  */
-load_plugin_textdomain(
-    'entrywiz', false,
-    basename( dirname( __FILE__ ) ) . '/languages'
-);
-
-
-function ewz_add_stylesheet() {
-    wp_register_style( 'ewz-style', plugins_url( 'styles/entrywizard.css', __FILE__) );
-    wp_enqueue_style( 'ewz-style' );
-
-    // Load the Javascript file
-    //  wp_register_script( "ewz-upload",
-    //                    plugins_url( 'javascript/ewz-upload.js', dirname(__FILE__) ),
-    //			array('jquery', 'jquery-form') );
-    wp_enqueue_script( 'ewz-upload',
-                        plugins_url( 'javascript/ewz-upload.js', __FILE__ ),
-                       array('jquery', 'jquery-form'),
-                       false,
-                       true      // in footer, so $ewzG has been defined
-                       );
-}
-
-function ewz_set_dev_env(){
-    if( is_dir( '/home/entrywizard' ) ){   // only true in development environment
-        /*
-         * ASSERT OPTIONS
-         */
-        assert_options( ASSERT_ACTIVE, true );
-        assert_options( ASSERT_BAIL, false );
-        assert_options( ASSERT_WARNING, false );
-        function ewz_assert_handler($file, $line, $code)
-        {
-            // no assert
-            error_log("EWZ: Assertion Failed at $file: $line: $code");
+function ewz_check_for_db_updates(){
+    $data             = get_plugin_data( __FILE__, false, false );
+    $this_version     = $data['Version'];
+    $ewz_data_version = get_option( 'ewz_data_version', '0.9.1' );
+    if ( version_compare( $ewz_data_version, $this_version,  '<' ) ){
+        // 0.9.6 added new apply_prefix column to webforms table 
+        // and changed 'min_img_area' to 'min_longest_dim' in fields/fdata
+        if ( version_compare( $ewz_data_version, '0.9.6', '<' ) ){
+            error_log( "EWZ: updating $ewz_data_version to 0.9.6" );
+            Ewz_Setup::activate_or_install_ewz();
+            delete_option( 'ewz_db_version' );
+            Ewz_Field::change_min_area_to_dim();    
         }
-        assert_options(ASSERT_CALLBACK, 'ewz_assert_handler');
-        define( 'EWZ_DBG', true );
-        define( 'CLEANUP_ON_DEACTIVATE', true );
-
-
-       error_log("~~~~~~~~ Starting entrywizard.php ( magic quotes not yet added ) ~~~~~~~ \n"
-                   . 'GET:   ' . print_r( $_GET, true )
-                . 'POST:  ' . print_r( $_POST, true )
-                . 'FILES: ' . print_r( $_FILES, true )
-                . 'URI:' . $_SERVER['REQUEST_URI']
-            );
-
-
-        include_once('Var_Dump.php');
-        Var_Dump::displayInit(array('display_mode' => 'HTML4_Table'), array('mode' => 'normal','offset' => 4));
-
-        function ewzdump( $obj ){
-            // no assert
-            $out = fopen( "/home/josie/dbg.html", 'a' );
-            fwrite( $out, Var_Dump::display($obj, true) );
-            fclose($out);
+        // 0.9.8 added attach_prefs to webform table
+        if ( version_compare( $ewz_data_version, '0.9.8', '<' ) ){
+            error_log( "EWZ: updating $ewz_data_version to 0.9.8" );
+            Ewz_Setup::activate_or_install_ewz();
         }
-
-    } else {
-        define( 'EWZ_DBG', false );
-        assert_options( ASSERT_ACTIVE, false );
-        define( 'CLEANUP_ON_DEACTIVATE', false );
+        // 1.0.0 added upload_date to item table
+        if ( version_compare( $ewz_data_version, '1.0.0', '<' ) ){
+            error_log( "EWZ: updating $ewz_data_version to 1.0.0" );
+            Ewz_Setup::activate_or_install_ewz();
+            Ewz_Item::set_upload_date();
+        }
+        // 1.1.0 added num_items to webform table and override to layout table
+        if ( version_compare( $ewz_data_version, '1.1.0', '<' ) ){
+            error_log( "EWZ: updating $ewz_data_version to 1.1.0" );
+            Ewz_Setup::activate_or_install_ewz();
+            Ewz_Webform::set_num_items();
+        }
+        // 1.2.0 added append to fields table
+        if ( version_compare( $ewz_data_version, '1.2.0', '<' ) ){
+            error_log( "EWZ: updating $ewz_data_version to 1.2.0" );
+            Ewz_Setup::activate_or_install_ewz();
+        }
+        // 1.2.3 added gen_fname to webforms table
+        if ( version_compare( $ewz_data_version, '1.2.3', '<' ) ){
+            error_log( "EWZ: updating $ewz_data_version to 1.2.3" );
+            Ewz_Setup::activate_or_install_ewz();
+        }
+        // 1.2.4 htaccess and index file in EWZ_IMG_UPLOAD_DIR
+        if ( version_compare( $ewz_data_version, '1.2.4', '<' ) ){
+            error_log( "EWZ: updating $ewz_data_version to 1.2.4" );
+            Ewz_Setup::protect_uploads();
+        }       
+        update_option( 'ewz_data_version', $this_version );
     }
 }
 
+
+function ewz_add_stylesheet() {
+    wp_register_style( 'ewz-style', plugins_url( 'styles/entrywizard.css', __FILE__ ) );
+    wp_enqueue_style( 'ewz-style' );
+
+    wp_enqueue_script(
+                       'ewz-upload',
+                       plugins_url( 'javascript/ewz-upload.js', __FILE__ ),
+                       array( 'jquery', 'jquery-form' ),
+                       false,
+                       true      // in footer, so $ewzG has been defined
+                      );
+    wp_enqueue_script(
+                       'ewz-followup',
+                       plugins_url( 'javascript/ewz-followup.js', __FILE__ ),
+                       array( 'jquery', 'jquery-form' ),
+                       false,
+                       true      // in footer, so $ewzG has been defined
+                      );
+ }
+
 function ewz_init_globals(){
     global $wpdb;
+
+    load_plugin_textdomain(
+                            'entrywiz', false,
+                            basename( dirname( __FILE__ ) ) . '/languages'
+                           );
+
     define( 'EWZ_PREFIX', $wpdb->prefix );
 
     define( 'EWZ_LAYOUT_TABLE',  EWZ_PREFIX . 'ewz_layout' );
@@ -158,24 +225,24 @@ function ewz_init_globals(){
 
 
     define( 'EWZ_DEFAULT_DIM',  1280 );           // default max image dimension in pixels
-    define( 'EWZ_DEFAULT_MIN_AREA',  100 );       // default smallest image area in square pixels
+    define( 'EWZ_DEFAULT_MIN_LONGEST',  100 );    // default minimum longest image dimension in pixels
     define( 'EWZ_MAX_STRING_LEN', 50 );           // default max length of text input field
     define( 'EWZ_MAX_FIELD_WIDTH', 10 );          // default max field width of text input field
 
-    define( 'EWZ_FILE_DOWNLOAD_TIME', 30 );       // number of seconds to add to max time allowance for every 50 files
+    define( 'EWZ_FILE_DOWNLOAD_TIME', 30 );       // number of seconds to add to max time allowance for every 25 files
     define( 'UPLOAD_ERR_EMPTY',   5 );            // error if uploaded file is empty -- see includes/ewz-upload.php
 
 
     $maxnumitems = ini_get( 'max_file_uploads' );
-    if( !$maxnumitems ){
+    if ( ! $maxnumitems ){
         $maxnumitems = 50;
     }
     $maxfilesize = ini_get( 'upload_max_filesize' );
-    if( !$maxfilesize ){
+    if ( ! $maxfilesize ){
         $maxfilesize = '5M';
     }
     $postmaxsize = ini_get( 'post_max_size' );
-    if( !$postmaxsize ){
+    if ( ! $postmaxsize ){
         $postmaxsize = '100M';
     }
 
@@ -188,73 +255,109 @@ function ewz_init_globals(){
 }
 
 
-    /**
-     * Transform a string like 100G or 50K to megabytes
-     *
-     * Works for an integer followed by 'm', 'mb', 'g','gb', 'k', 'kb'
-     * or the same in upper case
-     *
-     * @param   $sizestring
-     * @return  integer
-     */
-    function ewz_to_mb( $sizestring )
-    {
-        assert( is_string( $sizestring ) );
-        assert( preg_match( '/^(\d+)[mMgGkK][bB]?\s*$/', $sizestring ) );
+/**
+ * Transform a string like 100G or 50K to megabytes
+ *
+ * Works for an integer followed by 'm', 'mb', 'g','gb', 'k', 'kb'
+ * or the same in upper case
+ *
+ * @param   $sizestring
+ * @return  integer
+ */
+function ewz_to_mb( $sizestring )
+{
+    assert( is_string( $sizestring ) );
+    assert( preg_match( '/^(\d+)[mMgGkK][bB]?\s*$/', $sizestring ) );
 
-        $val = strtolower( trim( $sizestring ) );
-        $mat = array();
-        preg_match( '/^(\d+)(\D+)/', $val, $mat );
-        assert( 3 == count( $mat ) );
-        $num = $mat[1];
-        $code = $mat[2];
+    $val = strtolower( trim( $sizestring ) );
+    $mat = array();
+    preg_match( '/^(\d+)(\D+)/', $val, $mat );
+    assert( 3 == count( $mat ) );
+    $num  = $mat[1];
+    $code = $mat[2];
 
-        if ( preg_match( '/mb?/', $code ) ) {
-            return ( int )$num;
-        }
-        if ( preg_match( '/gb?/', $code ) ) {
-            return ( int )($num * 1024);
-        }
-        if ( preg_match( '/kb?/', $code ) ) {
-            return ( int )($num / 1024);
-        }
-        return 0;
+    if ( preg_match( '/mb?/', $code ) ) {
+        return ( int )$num;
     }
-
-
-    /**
-     * Transform a string like 100G or 50K to bytes
-     *
-     * Works for an integer followed by 'm', 'mb', 'g','gb', 'k', 'kb'
-     * or the same in upper case
-     *
-     * @param   $sizestring
-     * @return  integer
-     */
-    function ewz_to_bytes( $sizestring )
-    {
-        assert( is_string( $sizestring ) );
-        assert( preg_match( '/^(\d+)[mMgGkK][bB]?\s*$/', $sizestring ) );
-
-        $val = strtolower( trim( $sizestring ) );
-        $mat = array();
-        preg_match( '/^(\d+)(\D+)/', $val, $mat );
-        assert( 3 == count( $mat ) );
-        $num = $mat[1];
-        $code = $mat[2];
-
-        if ( preg_match( '/mb?/', $code ) ) {
-            return $num * 1024;
-        }
-        if ( preg_match( '/gb?/', $code ) ) {
-            return $num * 1048576;
-        }
-        if ( preg_match( '/kb?/', $code ) ) {
-            return $num;
-        }
-        return 0;
+    if ( preg_match( '/gb?/', $code ) ) {
+        return ( int )($num * 1024);
     }
+    if ( preg_match( '/kb?/', $code ) ) {
+        return ( int )($num / 1024);
+    }
+    return 0;
+}
 
+
+/**
+ * Transform a string like 100G or 50K to bytes
+ *
+ * Works for an integer followed by 'm', 'mb', 'g','gb', 'k', 'kb'
+ * or the same in upper case
+ *
+ * @param   $sizestring
+ * @return  integer
+ */
+function ewz_to_bytes( $sizestring )
+{
+    assert( is_string( $sizestring ) );
+    assert( preg_match( '/^(\d+)[mMgGkK][bB]?\s*$/', $sizestring ) );
+
+    $val = strtolower( trim( $sizestring ) );
+    $mat = array();
+    preg_match( '/^(\d+)(\D+)/', $val, $mat );
+    assert( 3 == count( $mat ) );
+    $num  = $mat[1];
+    $code = $mat[2];
+
+    if ( preg_match( '/mb?/', $code ) ) {
+        return $num * 1024;
+    }
+    if ( preg_match( '/gb?/', $code ) ) {
+        return $num * 1048576;
+    }
+    if ( preg_match( '/kb?/', $code ) ) {
+        return $num;
+    }
+    return 0;
+}
+
+/**
+ *
+ * Warn if required versions of WP and PHP not available
+ *
+ */
+function ewz_requires_version(){
+   $plugin = plugin_basename( EWZ_PLUGIN_DIR . '/entrywizard.php' );
+   $reqvers = ewz_required_versions_warning();
+   if ( $reqvers ) {
+       add_action( 'admin_notices', 'ewz_admin_notice' );
+   }     
+}
+   
+function ewz_required_versions_warning(){
+   global $wp_version;
+   $reqvers = '';
+
+   // version_compare returns -1 if left is lower than right, 0 if they are equal, 1 if right is lower than left
+   if ( version_compare( EWZ_REQUIRED_WP_VERSION, $wp_version ) == 1 ) {
+      $reqvers = 'WordPress version ' . EWZ_REQUIRED_WP_VERSION;
+      error_log("EWZ: wordpress version not supported");
+   }
+   if ( version_compare( EWZ_REQUIRED_PHP_VERSION, PHP_VERSION ) == 1 ) {
+       error_log("EWZ: php version not supported");
+      if( $reqvers ){
+            $reqvers .= " and ";
+      }
+      $reqvers .= 'PHP version ' . EWZ_REQUIRED_PHP_VERSION;
+   }
+   return $reqvers;
+}
+
+function ewz_admin_notice() {
+    $msg = ewz_required_versions_warning();
+    print "<div class='error'><h2>EntryWizard Warning</h2> EntryWizard requires at least <strong>$msg </strong></h2></div>";
+}
 
 
 /* * **************************************
@@ -270,11 +373,11 @@ function ewzdbg( $str, $in_obj = null )
 {
     if ( defined( 'EWZ_DBG' ) && EWZ_DBG ){
         // no assert
-        if ( $in_obj ) {
-            error_log(  "RHCC object $str: " . print_r( $in_obj, true ) );
+        if ( $in_obj !== null ) {
+            error_log(  "EWZ_DBG object $str: " . var_export( $in_obj, true ) );
             return;
         } else {
-            error_log( 'RHCC variable: ' .  print_r( $str, true ) );
+            error_log( 'EWZ_DBG variable: ' .  var_export( $str, true ) );
             return;
         }
     }
